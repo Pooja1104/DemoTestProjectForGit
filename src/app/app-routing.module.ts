2d8ed1dbbd2c994017c176801d8d877081ae7361
pooja1104 <yadavpuja2@gmail.com>
2023-10-07 21:46:18 +0530

    My first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FirstPageComponent } from './first-page/first-page.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    children:[
+      {
+        path:"firstpage",component:FirstPageComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+
+export class AppRoutingModule { }
